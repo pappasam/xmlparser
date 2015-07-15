3872f975616cd8b676c8cfadcce01a7bb6f33f56
Sam <samuel.roeca@gmail.com>
2015-07-15 12:09:55 -0400

    Update to make more functional

--- parse.scala
+++ parse.scala
@@ -2,36 +2,57 @@ import scala.io.Source
 import scala.xml.pull._
 
 object XmlParser {
-  val xml = new XMLEventReader(Source.fromFile("test.xml"))
-  val expectedHeaders = List("staff", "company")
 
-  def printText(text: String, currNode: List[String]) {
+  def getMap(text: String, currNode: List[String], m: Map[String, String], 
+    nodesIgnoreSet: Set[String]): Map[String, String] = 
     currNode match {
-      case _ if text.trim == "" => ()
-      case _ :: expectedHeaders => println(currNode.mkString(".") + ": " + text)
-      case _ => ()
+      case _ if text.trim == "" => m
+      case _ :: expectedHeaders => 
+        val name = currNode.filter(nodesIgnoreSet.contains(_) == false)
+        m + (name.mkString(".") -> text)
+      case _ => m
     }
-  }
 
-  def parse(xml: XMLEventReader) {
-    def loop(currNode: List[String]) {
+  def extractFromMap(m: Map[String, String], l: List[String]): List[String] = 
+    l.foldRight(Nil: List[String])((x,y) => m.getOrElse(x, "") :: y)
+
+  def parse(xml: XMLEventReader, nodesIgnore: List[String], 
+    nodesHeader: List[String], delimiter: String): String = {
+
+    val nodesIgnoreSet = nodesIgnore.toSet
+    val lineIdentifier = nodesIgnore.head
+
+    @annotation.tailrec
+    def loop(currNode: List[String], m: Map[String, String], 
+      l: List[String]): List[String] = {
       if (xml.hasNext) {
         xml.next match {
           case EvElemStart(_, label, _, _) =>
-            loop(label :: currNode)
-          case EvElemEnd(_, label) =>
-            loop(currNode.tail)
+            if (label == lineIdentifier) loop(label :: currNode, Map(), l)
+            else loop(label :: currNode, m, l)
+          case EvElemEnd(_, label) => 
+            if (label == lineIdentifier) {
+              val s = extractFromMap(m, nodesHeader).mkString(delimiter)
+              loop(currNode.tail, Map(), s :: l) // Output is reversed list
+            }
+            else
+              loop(currNode.tail, m, l)
           case EvText(text) =>
-            printText(text, currNode)
-            loop(currNode)
-          case _ => loop(currNode)
+            loop(currNode, getMap(text, currNode, m, nodesIgnoreSet), l)
+          case _ => loop(currNode, m, l)
         }
       }
+      else l
     }
-    loop(List.empty)
+    loop(List(), Map(), List()).mkString("\n")
   }
 
   def main(args: Array[String]) {
-    parse(xml)
+    val xml = new XMLEventReader(Source.fromFile("test.xml"))
+    val nodesIgnore = List("staff", "company")  
+    val nodesHeader = List("firstname", "lastname", "nickname", "salary", "year.birth")
+
+    val parsed = parse(xml, nodesIgnore, nodesHeader, ",")
+    println(parsed)
   }
 }
